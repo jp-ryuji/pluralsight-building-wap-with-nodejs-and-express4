b63fca813e350093b373ac014b2f264233dc9cbc
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-06 16:35:44 +0900

    Use morgan

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(morgan('tiny')); // 'combined' shows more informatoin.
 
 app.get('/', (req, res) => {
   res.send('Hello from my app');
