8d5c5aa5f69cce025ed1903118c59de31ac30dc9
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-06 17:36:44 +0900

    Serve js and css files from node_modules dir

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(morgan('tiny')); // 'combined' shows more informatoin.
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
