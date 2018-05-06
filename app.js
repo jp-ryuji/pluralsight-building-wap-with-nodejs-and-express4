01c23c419139bdd3225d460eb8bfd3e86870caad
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-06 16:43:44 +0900

    Use path to concatinate paths

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(morgan('tiny')); // 'combined' shows more informatoin.
 
 app.get('/', (req, res) => {
-  res.send('Hello from my app');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(port, () => {
