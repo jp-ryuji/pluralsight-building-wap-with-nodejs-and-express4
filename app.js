728984cf8e5159ffee681e29fa8cc8cbafd5b2fe
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-08 20:14:08 +0900

    Implement router with arguments

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+
 app.use(morgan('tiny')); // 'combined' shows more informatoin.
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -16,6 +17,10 @@ app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
 
+const nav = [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }];
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
   res.render('index', {
     nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
