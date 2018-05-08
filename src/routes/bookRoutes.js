82ff6cdefff5816f190d8765b01c1c8c1a2a5afd
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-08 22:48:19 +0900

    bookRoutes returns single record

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const debug = require('debug')('app:bookRoutes');
 
 const bookRouter = express.Router();
@@ -19,6 +19,27 @@ function router(_nav) {
           .find()
           .toArray();
         res.json(books);
+      } catch (err) {
+        debug(err.stack);
+      }
+
+      client.close();
+    })();
+  });
+
+  bookRouter.route('/:id').get((req, res) => {
+    const { id } = req.params;
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const book = await db.collection('books').findOne({ _id: new ObjectId(id) });
+        debug(book);
+        res.json(book);
       } catch (err) {
         debug(err.stack);
       }
