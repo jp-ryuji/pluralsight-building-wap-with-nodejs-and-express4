bf1c4ade6e80fb85b301f1d4ff489ce9db03f33c
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-09 09:23:02 +0900

    Add authRoutes.js

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(morgan('tiny')); // 'combined' shows more informatoin.
+app.use(bodyParser.json()); // bodyParser.json() is a middleware. So without the last (), it doesn't work.
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
@@ -20,9 +23,11 @@ const port = process.env.PORT || 3000;
 const nav = [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }];
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 app.get('/', (req, res) => {
   res.render('index', {
     nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
